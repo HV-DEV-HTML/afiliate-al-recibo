404ab2ba264d6c0ce5a8edd9e8fc906104d0d87f
victorcastrohavas <164097912+victorcastrohavas@users.noreply.github.com>
2024-06-06 09:42:29 -0500

    add landing bbva

--- js/main-bbva.js
+++ js/main-bbva.js
@@ -17,18 +17,50 @@ const isNumber = (evt) => {
   evt = evt || window.event;
   const charCode = evt.which || evt.keyCode;
 
-  // Permitir teclas de control como Enter, retroceso y eliminar
+  // Permitir teclas de control como Enter (13), retroceso (8) y eliminar (46)
   if (charCode === 13 || charCode === 8 || charCode === 46) {
     return true;
   }
 
-  // Asegurar que el carácter sea un dígito del 0 al 9
+  // Asegurar que el carácter sea un dígito del 0 al 9 (charCode entre 48 y 57)
   if (charCode < 48 || charCode > 57) {
     evt.preventDefault();
     return false;
   }
 
   return true;
+};
+
+const isNumberKey = (evt) => {
+  const charCode = evt.which || evt.keyCode;
+
+  // Permitir teclas de control como Enter, retroceso, eliminar, tabulación, flechas
+  if (
+    charCode === 13 || // Enter
+    charCode === 8 ||  // Backspace
+    charCode === 46 || // Delete
+    charCode === 9 ||  // Tab
+    (charCode >= 37 && charCode <= 40) // Flechas
+  ) {
+    return true;
+  }
+
+  // Asegurar que el carácter sea un dígito del 0 al 9 (teclas superiores) o del teclado numérico
+  const isNumber = 
+    (charCode >= 48 && charCode <= 57) || // Números del 0 al 9
+    (charCode >= 96 && charCode <= 105); // Números del teclado numérico
+
+  if (!isNumber) {
+    evt.preventDefault();
+    return false;
+  }
+
+  return true;
+};
+
+const preventNonNumericInput = (evt) => {
+  const input = evt.target;
+  input.value = input.value.replace(/[^0-9]/g, '');
 };
 
 const isAlphanumeric = (inputString) => {
@@ -38,11 +70,42 @@ const isAlphanumeric = (inputString) => {
   }
   return false;
 }
+const isAlphanumericKey = (evt) => {
+  const charCode = evt.which || evt.keyCode;
+
+  // Permitir teclas de control como Enter, retroceso, eliminar, tabulación, flechas
+  if (
+    charCode === 13 || // Enter
+    charCode === 8 ||  // Backspace
+    charCode === 46 || // Delete
+    charCode === 9 ||  // Tab
+    (charCode >= 37 && charCode <= 40) // Flechas
+  ) {
+    return true;
+  }
+
+  // Obtener el carácter desde el evento
+  const char = String.fromCharCode(charCode);
+
+  // Usar una expresión regular para permitir solo letras (a-z, A-Z) y números (0-9)
+  const isValidChar = /^[a-zA-Z0-9]$/.test(char);
+
+  if (!isValidChar) {
+    evt.preventDefault();
+    return false;
+  }
+
+  return true;
+};
+
+const preventNonAlphanumericInput = (evt) => {
+  const input = evt.target;
+  input.value = input.value.replace(/[^a-zA-Z0-9]/g, '');
+};
 
 const onlyStrings = (event) => {
   const inputText = event.target.value;
   const regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*$/;
-  console.log(inputText)
   if (!regex.test(inputText)) {
     event.target.value = inputText.replace(/[^a-zA-ZñÑáéíóúÁÉÍÓÚ\s]/g, '');
   }
@@ -84,7 +147,7 @@ const validPhone = (name) => {
 }
 
 const validateDocument = (value, kind) => {
-
+  // console.log(kind.type_documento.value)
   if (kind.type_documento.value === "DNI") {
     if (value.split("").length === 8) {
       return true;
@@ -93,9 +156,8 @@ const validateDocument = (value, kind) => {
     }
   }
 
-
-  if (kind.type_documento.value === "Pasaporte") {
-    if (value.split("").length === 12) {
+  if (kind.type_documento.value == "Pasaporte") {
+    if (value.split("").length >= 9) {
       return true;
     } else {
       return false;
@@ -130,6 +192,44 @@ const validateDocument = (value, kind) => {
     }
   }
 }
+const validateService = (value, kind) => {
+  if (kind.type_service.value === "Linea-movil") {
+    if (value.split("").length >= 9 & validPhone(value)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (kind.type_service.value === "Linea-fija") {
+    if (value.split("").length >= 8 ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (kind.type_service.value === "Codigo-de-servicio") {
+    if (value.split("").length >= 6 ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const changeTypeServices = (event) =>{
+  const modalTypeService = document.querySelector('.portlet[data-portlet=bannerform] > .section > #modalTypeService')
+  if (event.target.value == 'Linea-fija') {
+    modalTypeService.classList.add("active")
+  }
+}
+
+const openModalBBVA = (event) => {
+  const modalBBVA = document.querySelector('.portlet[data-portlet=bannerform] > .section > #modalBBVA')
+  if (!modalBBVA.classList.contains("opened")) {
+    modalBBVA.classList.add("opened")
+    modalBBVA.classList.add("active")
+  }
+}
 
 const modal1 = ref(false);
 const modal2 = ref(false);
@@ -149,9 +249,12 @@ const app = createApp({
     onMounted(() => {
       register1.nombres = "";
       register1.type_documento = '';
+      register1.type_service = '';
       register1.documento = "";
+      register1.servicio = "";
       register1.correo = "";
       register1.celular = "";
+      register1.cuenta_bancaria_code = "";
       register1.cuenta_bancaria = "";
       register1.tratamiento = false;
       register1.promociones = false;
@@ -161,62 +264,82 @@ const app = createApp({
     const register1 = reactive({
       nombres: "",
       type_documento: '',
+      type_service: '',
       documento: "",
+      servicio: "",
       correo: "",
       celular: "",
+      cuenta_bancaria_code: "",
       cuenta_bancaria: "",
       tratamiento: false,
       promociones: false,
     });
 
-    const rules1 = {
-      nombres: {
+    const rules1 = computed(() => {
+      const documentoRules = {
         required,
         name_validation: {
-          $validator: validName,
+          $validator: validateDocument,
         },
-      },
-      type_documento: {
+      };
+      
+      if (register1.type_documento === 'DNI' || register1.type_documento === 'CE') {
+        documentoRules.numeric = numeric;
+      } else {
+        documentoRules.alphaNum = alphaNum;
+      }
+      const servicesRules = {
         required,
-      },
-      documento: {
-        required,
-        alphaNum,
         name_validation: {
-          $validator: validateDocument
-        }
-      },
-      celular: {
-        required,
-        // numeric,
-        // name_validation: {
-        //   $validator: validPhone,
-        // },
-        min: minLength(9),
-      },
-      cuenta_bancaria: {
-        required,
-        numeric,
-        min: minLength(20),
-      },
-      correo: {
-        required,
-        //email
-        name_validation: {
-          $validator: validEmail,
+          $validator: validateService,
         },
-      },
-      tratamiento: {
-        required(val) {
-          return val;
+      };
+      
+      if (register1.type_service === 'Linea-movil' || register1.type_service === 'Linea-fija') {
+        servicesRules.numeric = numeric;
+      } else {
+        servicesRules.alphaNum = alphaNum;
+      }
+
+      return {
+        nombres: {
+          required,
+          name_validation: {
+            $validator: validName,
+          },
         },
-      },
-      promociones: {
-        /*required(val) {
-          return val
-        }*/
-      },
-    };
+        type_documento: {
+          required,
+        },
+        type_service: {
+          required,
+        },
+        documento: documentoRules,
+        servicio: servicesRules,
+        cuenta_bancaria_code: {
+          required,
+          numeric,
+          min: minLength(4),
+        },
+        cuenta_bancaria: {
+          required,
+          numeric,
+          min: minLength(9),
+        },
+        correo: {
+          required,
+          name_validation: {
+            $validator: validEmail,
+          },
+        },
+        tratamiento: {
+          required(val) {
+            return val;
+          },
+        },
+
+      };
+    });
 
     const lgthTypeDocument = computed(() => {
       switch (register1.type_documento) {
@@ -225,20 +348,35 @@ const app = createApp({
         case "RUC":
           return 11;
         case "Pasaporte":
-          return 12;
+          return 20;
         case "CE":
-          return 12;
+          return 20;
         case "":
           return 0;
       }
     })
+    const lengthTypeService = computed(() => {
+      switch (register1.type_service) {
+        case "Linea-movil":
+          return 9;
+        case "Linea-fija":
+          return 9;
+        case "Codigo-de-servicio":
+          return 14;
+        case "":
+          return 0;
+      }
+    });
 
     const v1$ = useVuelidate(rules1, {
       nombres: toRef(register1, "nombres"),
       type_documento: toRef(register1, "type_documento"),
+      type_service: toRef(register1, "type_service"),
       documento: toRef(register1, "documento"),
+      servicio: toRef(register1, "servicio"),
       correo: toRef(register1, "correo"),
       celular: toRef(register1, "celular"),
+      cuenta_bancaria_code: toRef(register1, "cuenta_bancaria_code"),
       cuenta_bancaria: toRef(register1, "cuenta_bancaria"),
       tratamiento: toRef(register1, "tratamiento"),
       promociones: toRef(register1, "promociones"),
@@ -250,6 +388,8 @@ const app = createApp({
 
       if (v1$.value.$invalid) return;
       disabled.value = true;
+      const inputCuenta = document.querySelector(".formBC__main-input[name='numero_de_cuenta_bancaria']")
+      inputCuenta.value = '0011' + register1.cuenta_bancaria_code + register1.cuenta_bancaria
       document.getElementById("btnAction").classList.remove('active')
 
       form1.value.submit();
@@ -257,24 +397,36 @@ const app = createApp({
       setTimeout(() => {
         register1.nombres = "";
         register1.type_documento = '';
+        register1.type_service = '';
         register1.documento = "";
+        register1.servicio = "";
         register1.correo = "";
         register1.celular = "";
+        register1.cuenta_bancaria_code = "";
         register1.cuenta_bancaria = "";
         register1.tratamiento = false;
         register1.promociones = false;
         disabled.value = false;
-      }, 3000)
+        v1$.value.$reset();
+        document.querySelector(".formBC__main-input[name='monto_maximo']").value = "";
+      }, 1000)
     };
 
     return {
       register1,
       submitForm1,
       isNumber,
+      isNumberKey,
+      preventNonNumericInput,
       isAlphanumeric,
+      isAlphanumericKey,
+      preventNonAlphanumericInput,
       validName,
       onlyStrings,
       onlyStringsKey,
+      validPhone,
+      changeTypeServices,
+      openModalBBVA,
       form1,
       v1$,
       modal1,
@@ -282,7 +434,8 @@ const app = createApp({
       modal3,
       disabled,
       limit,
-      lgthTypeDocument
+      lgthTypeDocument,
+      lengthTypeService,
     };
   },
 });
